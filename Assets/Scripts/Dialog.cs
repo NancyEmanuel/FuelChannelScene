fcd921b9f89109d2b58a9eb7bd8b7941ef98b7e6
Nancy Emmanuel <76671907+NancyEmanuel@users.noreply.github.com>
2021-06-21 12:04:28 -0400

    commit

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour{
+
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type(){
+
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void DisplayText()
+    {
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+
+    }
+}
